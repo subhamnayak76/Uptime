96be0834b249eaec89037354bff6f1d5b753c0c6
subhamnayak76 <subhamnayak9638@gmail.com>
2025-04-18 13:56:01 +0530

    v2

--- dist/service/emailworker.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// subscriber-runner.ts
-const subscriber_1 = require("../pubsub/subscriber");
-(0, subscriber_1.subscribetoEmailEvents)();
